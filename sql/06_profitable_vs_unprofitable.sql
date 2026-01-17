2e5b42133de79c1bfa261f198e9853b008d97c02
anirudhs2u <anirudhsu@gmail.com>
2026-01-17 19:23:13 +0530

    Compare loss-making Tables with profitable sub-category

--- sql/06_profitable_vs_unprofitable.sql
+++ sql/06_profitable_vs_unprofitable.sql
@@ -1,1 +1,11 @@
+-- Comparison between loss-making and profitable sub-categories
+-- Goal: Understand differences in sales, profit, and discount sensitivity
 
+SELECT sub_category,
+ROUND(AVG(discount), 2) AS avg_discount,
+SUM(sales) AS total_sales,
+SUM(profit) AS total_profit,
+ROUND(SUM(profit) / NULLIF(SUM(sales), 0) * 100, 2) AS profit_margin_pct
+FROM superstore
+WHERE sub_category IN ('Tables', 'Chairs')
+GROUP BY sub_category;
