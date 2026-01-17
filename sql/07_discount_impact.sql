9edb0acfdc89d61ed7909ee72accc72f3d6d96d5
anirudhs2u <anirudhsu@gmail.com>
2026-01-17 19:27:44 +0530

    Analyze discount impact on profitability across sub-categories

--- sql/07_discount_impact.sql
+++ sql/07_discount_impact.sql
@@ -1,1 +1,13 @@
+-- Impact of discounting on profitability across sub-categories
+-- Goal: Identify discount levels that lead to margin erosion
+
+SELECT sub_category,
+ROUND(discount, 2) AS discount_level,
+COUNT(*) AS order_count,
+SUM(sales) AS total_sales,
+SUM(profit) AS total_profit,
+ROUND(SUM(profit) / NULLIF(SUM(sales), 0) * 100, 2) AS profit_margin_pct
+FROM superstore
+GROUP BY sub_category, discount_level
+ORDER BY sub_category, discount_level;
 
